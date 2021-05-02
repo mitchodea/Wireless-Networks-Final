a7fd4cc69d7904b26517fadd5b279534e8efb7f7
mitchodea <mitchodea@gmail.com>
2021-05-03 00:36:48 +1000

    James weird code / Added new Xbee Router Conf

--- Arduino/serial_reader/serial_reader.ino
+++ Arduino/serial_reader/serial_reader.ino
@@ -1,23 +1,23 @@
 #include <Adafruit_BMP280.h>
 
 struct Transmission {
-    private:
         uint8_t _start_byte{0xaa};
         int8_t node_id{1};
-
-    public:
         int8_t v1;
         int16_t v2;
         float v3;
-
-    private:
         uint8_t _end_byte{0xee};
+};
+
+union hahalmao {
+  Transmission t;
+  uint8_t b[sizeof(Transmission)];
 }
 
 Adafruit_BMP280 bmp; // I2C Interface
 int potPin = 2;    // select the input pin for the potentiometer
 
-Transmission transmission{0,0,0};
+hahalmao transmission;
 
 int8_t test_counter{0};
 float test_float{1};
@@ -32,12 +32,14 @@ void setup() {
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  transmission.v2 = 0;
 }
 
 void loop() {
-    transmission.v1 = ++testcounter;
+    transmission.v1 = ++test_counter;
     test_float *= 1.01;
     transmission.v3 = test_float;
-    Serial.write(static_cast<char*>transmission, sizeof(Transmission));
+    
+    Serial.write(send_buffer, sizeof(Transmission));
     delay(1000);
 }
