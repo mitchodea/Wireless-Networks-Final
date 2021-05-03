5842a0ee8fc91c3d08b4d22a66676676a49715ea
James Stow <jamesstow2@gmail.com>
2021-05-03 15:53:06 +1000

    node 02

--- Arduino/serial_reader/node_02.ino
+++ Arduino/serial_reader/node_02.ino
@@ -4,14 +4,14 @@
 struct Transmission {
     private:
         uint8_t _start_byte{0xaa};
-        int8_t node_id{3};
+        int8_t node_id{2};
     public:
-        float temp = 0.0;
-        float humidity = 0.0;
-        float air_quality = 0.0;
-        float flame = 0.0;
-        float elevation = 0.0;
-        float pressure = 0.0;
+        float temp = 0.0f;
+        float humidity = 0.0f;
+        float air_quality = 0.0f;
+        float flame = 0.0f;
+        float elevation = 0.0f;
+        float pressure = 0.0f;
 
     private:
         uint8_t _end_byte{0xee};
@@ -24,6 +24,8 @@ union u_t {
 
 const int tempHum_pin{7};
 DHT tempHum(tempHum_pin, DHT11);
+const int flame_pin{A5};
+const int air_q_pin{A4};
 
 u_t transmission{Transmission()};
 SoftwareSerial XBee(1, 0);
@@ -35,7 +37,14 @@ void setup() {
 
 void loop() {
     transmission.t.temp = tempHum.readTemperature();
+    if (isnan(transmission.t.temp)) transmission.t.temp = 0.0f;
+
     transmission.t.humidity = tempHum.readHumidity();
+    if (isnan(transmission.t.humidity)) transmission.t.humidity = 0.0f;
+
+    transmission.t.flame = analogRead(flame_pin);
+    transmission.t.air_quality = analogRead(air_q_pin);
+
     XBee.write(+transmission.b, sizeof(Transmission));
     delay(1000);
 }
