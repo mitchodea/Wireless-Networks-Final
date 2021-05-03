73c2d7a8f72acf7b4fe805ac9ec06f3c3ea2361a
James Stow <jamesstow2@gmail.com>
2021-05-03 17:04:45 +1000

    doneski

--- Arduino/serial_reader/node_01.ino
+++ Arduino/serial_reader/node_01.ino
@@ -1,10 +1,21 @@
 #include <SoftwareSerial.h>
 #include <DHT.h>
+#include <Adafruit_BMP280.h>
+
+#define TEMP_HUM 7
+
+#define FLAME A5
+#define AIR_Q A4
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
 
 struct Transmission {
     private:
         uint8_t _start_byte{0xaa};
-        int8_t node_id{2};
+        int8_t node_id{1};
     public:
         float temp = 0.0f;
         float humidity = 0.0f;
@@ -22,10 +33,8 @@ union u_t {
   uint8_t b[sizeof(Transmission)];
 };
 
-const int tempHum_pin{7};
-DHT tempHum(tempHum_pin, DHT11);
-const int flame_pin{A5};
-const int air_q_pin{A4};
+DHT tempHum(TEMP_HUM, DHT11);
+Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 u_t transmission{Transmission()};
 SoftwareSerial XBee(1, 0);
@@ -33,6 +42,7 @@ SoftwareSerial XBee(1, 0);
 void setup() {
     XBee.begin(9600);
     tempHum.begin();
+    bmp.begin();
 }
 
 void loop() {
@@ -42,8 +52,13 @@ void loop() {
     transmission.t.humidity = tempHum.readHumidity();
     if (isnan(transmission.t.humidity)) transmission.t.humidity = 0.0f;
 
-    transmission.t.flame = analogRead(flame_pin);
-    transmission.t.air_quality = analogRead(air_q_pin);
+    transmission.t.flame = analogRead(FLAME);
+    transmission.t.flame = (
+        (transmission.t.flame == 0.0) ? 0.0 : 1.0/transmission.t.flame
+    );
+
+    transmission.t.pressure = bmp.readPressure();
+    transmission.t.elevation = bmp.readAltitude();
 
     XBee.write(+transmission.b, sizeof(Transmission));
     delay(1000);
